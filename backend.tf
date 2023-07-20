bf730a376c5b4233a9a94228abbd1f715b145f87
kakarlavenu <137424408+nubiiojm@users.noreply.github.com>
2023-07-20 10:24:42 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,4 +1,9 @@
-terraform {
+
+provider "aws" {
+   region     = "ap-south-1"
+   access_key = "AKIA3R2UVGLMTWGIXGGE"
+   secret_key = "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"
+} terraform {
   backend "s3" {
     bucket = "venujenkins11"
     key = "main"
